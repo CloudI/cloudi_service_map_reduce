fa1b4bfe517248bd134d0c3f5d660a9d6899914c
Michael Truog <mjtruog@gmail.com>
2014-10-30 18:59:59 -0700

    Update hex package.

---
+++ mix.exs
@@ -0,0 +1,29 @@
+defmodule CloudIServiceMapReduce do
+  use Mix.Project
+
+  def project do
+    [app: :cloudi_service_map_reduce,
+     version: "1.4.0-rc.4",
+     language: :erlang,
+     description: description,
+     package: package,
+     deps: deps]
+  end
+
+  defp deps do
+    [{:cloudi_core, "~> 1.4.0-rc.4"}]
+  end
+
+  defp description do
+    "Erlang/Elixir Cloud Framework Map/Reduce Service"
+  end
+
+  defp package do
+    [files: ~w(src doc rebar.config README.markdown),
+     contributors: ["Michael Truog"],
+     licenses: ["BSD"],
+     links: %{"Website" => "http://cloudi.org",
+              "GitHub" => "https://github.com/CloudI/" <>
+                          "cloudi_service_map_reduce"}]
+   end
+end
