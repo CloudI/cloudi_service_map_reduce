87bdff13eb4084d5308dec53f398ffe1177b015e
Michael Truog <mjtruog@gmail.com>
2017-06-07 12:56:20 -0700

    Version 1.7.1.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule CloudIServiceMapReduce do
 
   def project do
     [app: :cloudi_service_map_reduce,
-     version: "1.7.0",
+     version: "1.7.1",
      language: :erlang,
      description: description(),
      package: package(),
@@ -14,7 +14,7 @@ defmodule CloudIServiceMapReduce do
   end
 
   defp deps do
-    [{:cloudi_core, "~> 1.7.0"}]
+    [{:cloudi_core, "~> 1.7.1"}]
   end
 
   defp description do
